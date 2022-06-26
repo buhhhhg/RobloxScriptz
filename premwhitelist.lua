20c832f0b64327c78a4f2d99e12253c90b59eee0
C5Hax <108026120+C5Hax@users.noreply.github.com>
2022-06-26 19:50:34 +0500

    Update premwhitelist.lua

--- premwhitelist.lua
+++ premwhitelist.lua
@@ -1,7 +1,25 @@
+local prefix = K5!!
+
 local premUser = {
   "",
+  ""
 }
 
 --
 
 -- Premium Users
+-- credit to maxbrh1 / MastersMZ | the site i got the code from is https://controlc.com/5a7a6fe2
+LPlayer.Chatted:Connect(function(msg)
+	msg = msg:lower()
+	if string.sub(msg,1,3) == "/e " then
+		msg = string.sub(msg,4)
+	end
+	if string.sub(msg,1,1) == prefix then
+		local cmd
+		local space = string.find(msg," ")
+		if space then
+			cmd = string.sub(msg,2,space-1)
+		else
+			cmd = string.sub(msg,2)
+		end
+--
