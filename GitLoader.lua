e0e72fc9428f47de711e0353fb5e4584880129f4
C5Hax <108026120+C5Hax@users.noreply.github.com>
2022-07-08 09:38:33 +0500

    Create GitLoader.lua
    credits to bloodball on github lol

---
+++ GitLoader.lua
@@ -0,0 +1,42 @@
+-- K5's GitLoader
+-- Credits to bloodball
+
+local Lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/sol"))()
+
+local win = Lib:New({
+  Name = "K5 GitLoader",
+  FolderToSave = "GitLoader"
+})
+
+local tab = win:Tab("Hubs")
+
+local sec = tab:Section("Scripts")
+
+sec:Button("Arctic Hub Remade", function()
+	Lib:Notification("GitLoader", "Loaded Arctic Hub Remade.")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/C5Hax/RobloxScriptz/main/Arctic%20Hub%20Remade"))()
+end)
+
+sec:Button("K5Hub", function()
+	Lib:Notification("GitLoader", "Loaded K5Hub.")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/C5Hax/RobloxScriptz/main/K5Hub.lua"))()
+end)
+
+sec:Button("C5Hub DH", function()
+	Lib:Notification("GitLoader", "Loaded C5Hub DH.")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/C5Hax/RobloxScriptz/main/C5Hub%20Da%20Hood"))()
+end)
+
+sec:Button("C5Hub Universal", function()
+	Lib:Notification("GitLoader", "Loaded C5Hub Universal.")
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/C5Hax/RobloxScriptz/main/C5Hub%20Universal.lua"))()
+end)
+--
+local tab2 = win:Tab("Other Scripts/Executors")
+
+local sec2 = tab:Section("Executors")
+
+sec2:Button("Moonify Executor", function()
+  Lib:Notification("GitLoader", "Set your clipboard to download link.")
+  setclipboard("https://moonify.lol/home")
+end)
