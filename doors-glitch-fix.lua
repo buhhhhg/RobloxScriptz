57c2f70c6208bb78672074da3da1ffc370761737
K5 <108026120+C5Hax@users.noreply.github.com>
2023-10-28 02:25:41 +0500

    Update doors-glitch-fix.lua

--- doors-glitch-fix.lua
+++ doors-glitch-fix.lua
@@ -20,12 +20,10 @@ notif('creds', 'shit script made by k5utils', 10)
 
 notif('bruh', 'note that this is very useless ngl', 2)
 if figurecamposplayer then
-	notif('how?', 'patching glitch - figurecampos ...', 2.6)
-	notif('how?', '... player found.', 2.6)
+	notif('how?', 'patching glitch', 2.6)
 	figurecamposchar:Destroy()
 	wait()
-	notif('bruh?', 'patched glitch, you can ...', 2.6)
-	notif('bruh?', '... pass door 50 now!', 2.6)
+	notif('bruh?', 'patched glitch', 2.6)
 else
 	notif('noob', 'FigureCamPos player not found', 3.5)
 end
