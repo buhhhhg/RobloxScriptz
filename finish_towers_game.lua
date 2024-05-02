06be65bfa3f4b486bb3a3bea18caf08b1486f4c3
C5Hax <nauhanali420@gmail.com>
2024-05-03 00:14:12 +0500

    swap to touch interest

--- finish_towers_game.lua
+++ finish_towers_game.lua
@@ -25,11 +25,9 @@ end
 
 notif("Credits", "Script made by k5utils on discord", 3)
 
-character = game.Players.LocalPlayer.Character
-
 for i,v in levels do
-	character.HumanoidRootPart.CFrame = workspace["TowersModel"][v]["EndPortal"]["Field"].CFrame
-	wait(1.5)
+	firetouchinterest(workspace["TowersModel"][v]["EndPortal"]["Field"]["TouchInterest"])
+	wait()
 end
 
 notif("Buh.", "Finished all stages", 3)
